5e42247e514cac61dba9cd6e402a67a542419abb
Enzo Borrelli <johndoe@example.comenzoborrellidev@gmail.com>
2023-01-20 16:19:59 -0300

    setting the workspace

---
+++ Mod forge workspace/src/main/java/com/enderdev/soulswithin/SoulsWithin.java
@@ -0,0 +1,42 @@
+package com.enderdev.soulswithin;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+
+@Mod(SoulsWithin.MOD_ID)
+public class SoulsWithin {
+
+    public static final String MOD_ID = "soulswithin";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public SoulsWithin() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
